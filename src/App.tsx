81ccb4525b1f23a9381fb881bc1ae3d9942fa706
Nathã Hernandez <nathahcorrea@gmail.com>
2022-09-07 23:02:35 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+const App = () => {
+  return (
+    <div className="App">
+      
+      <div>
+          <input placeholder='Digite aqui...'/>    
+      </div>
+
+    </div>
+  )
+}
+
+export default App
